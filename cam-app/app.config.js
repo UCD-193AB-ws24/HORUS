3cfdfa6e2f1127befe8b3d93f12986c6714188d6
anorak-lightman <164287745+anorak-lightman@users.noreply.github.com>
2025-05-19 11:35:21 -0700

    Fixing dependencies

--- cam-app/app.config.js
+++ cam-app/app.config.js
@@ -6,4 +6,21 @@ export default ({ config }) => ({
     SUPABASE_URL: process.env.SUPABASE_URL,
     SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
   },
+  expo: {
+    ...config.expo,
+    extra: {
+      ...config.expo?.extra, 
+      apiUrl: process.env.EXPO_PUBLIC_API_URL,
+      eas: {
+        projectId: "50db876c-6aa5-4b1f-8d55-e9c24f2270f2",
+        EAS_SKIP_AUTO_FINGERPRINT: "1"
+      }
+    },
+    ios: {
+      bundleIdentifier: "com.ecs193.signlanguageapp",
+      infoPlist: {
+        ITSAppUsesNonExemptEncryption: "false"
+      }
+    }
+  }
 });
